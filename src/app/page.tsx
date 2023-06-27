78fed5b894a619b7c13c840f5abcb34d563d4ffd
Petar Dzhambazov <jamby77@gmail.com>
2023-06-28 00:42:52 +0300

    Add countdown logic and animation to Home component
    The changes introduced in this commit involve including countdown logic in the Home component. Additionally, an animation effect has been added that gets triggered when the countdown is below or equal to 3. Two hooks have been used, one for decrementing the state value by 1 every second till it reaches 0. The other hook triggers a timer to display 'GO' after the countdown reaches 0. React state and effects have been used to achieve this functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,61 @@
-import { Timer } from '@/components/timer/Timer'
+"use client";
+
+import React, { useEffect } from "react";
+
+import { Timer } from "@/components/timer/Timer";
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Timer />
-        </main>
-    )
+  const [state, setState] = React.useState(10);
+  const [showGo, setShowGo] = React.useState(false);
+  // using requestAnimationFrame instead of setTimeout increment state by 1 every second
+  useEffect(() => {
+    let lastTime = performance.now();
+    let requestId: number;
+
+    const loop = (currentTime: number) => {
+      const deltaTime = currentTime - lastTime;
+
+      if (deltaTime >= 1000) {
+        // 1000 ms = 1 sec
+        setState((state) => {
+          if (state <= 0) {
+            return 0;
+          }
+          return state - 1;
+        });
+        lastTime = currentTime;
+      }
+      if (state > 0) {
+        requestId = requestAnimationFrame(loop);
+      }
+    };
+
+    requestId = requestAnimationFrame(loop);
+
+    return () => cancelAnimationFrame(requestId);
+  }, [state]);
+
+  // when state = 0, set a 1000 ms timer to set shoGo to true
+  useEffect(() => {
+    if (state === 0) {
+      const timer = setTimeout(() => {
+        setShowGo(true);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [state]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div
+        className={`text-9xl font-bold text-sky-500 ${
+          state <= 3 && !showGo ? "animate-ping" : "animate-none"
+        }`}
+      >
+        {!showGo && state}
+        {showGo && "GO"}
+      </div>
+      <Timer />
+    </main>
+  );
 }
