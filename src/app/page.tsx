6034becfa37ccf088fb75c43a3c91dedf4908382
jamby77 <jamby77@gmail.com>
2025-11-16 17:35:49 +0200

    comment our timer and stopwatch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Timer } from "@/components/Timer";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-10 bg-gray-100 p-4">
-      <Timer duration={10} label="Work" completionMessage="Time's up!" />
-      <Stopwatch label="Stopwatch" timeLimit={5} completionMessage="Time limit reached" />
+      {/*<Timer duration={10} label="Work" completionMessage="Time's up!" />*/}
+      {/*<Stopwatch label="Stopwatch" timeLimit={5} completionMessage="Time limit reached" />*/}
       <Interval
         intervalConfig={{
           workDuration: 20,
